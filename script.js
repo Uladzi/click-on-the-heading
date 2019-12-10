c5f45957a104cfb067f3414aa496333b3ea121d7
Uladzi <bivmak@gmail.com>
2019-12-10 15:40:42 +0300

    feat: add some corrects

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let direction = 'right';
 let offset = 0;
 
-$('h1').offset({ left: offset }).offset({ top: offset });
+$('img').offset({ left: offset }).offset({ top: offset });
 
 const squareAnimation = function() {
     if (direction === 'right') {
-        $('h1').offset({ left: offset });
+        $('img').offset({ left: offset });
         offset++;
         if (offset > 250) {
             offset = 0;
             direction = 'down';
         }
     } else if (direction === 'down') {
-        $('h1').offset({ top: offset });
+        $('img').offset({ top: offset });
         offset++;
         if (offset > 250) {
             offset = 250;
             direction = 'left';
         }
     } else if (direction === 'left') {
-        $('h1').offset({ left: offset });
+        $('img').offset({ left: offset });
         offset--;
         if (offset < 0) {
             offset = 250;
             direction = 'up';
         }
     } else if (direction === 'up') {
-        $('h1').offset({ top: offset });
+        $('img').offset({ top: offset });
         offset--;
         if (offset < 0) {
             offset = 0;
@@ -36,7 +36,7 @@ const squareAnimation = function() {
 };
 
 let intervalLength = 30;
-setInterval(squareAnimation, intervalLength);
+const intervalID = [setInterval(squareAnimation, intervalLength)];
 let clicks = 10;
 
 const speedUpIntervalFunc = function() {
@@ -44,13 +44,13 @@ const speedUpIntervalFunc = function() {
     clicks--;
     $('h1').text(clicks);
     if (clicks < 1) {
-        for (let i = 1; i <= 10; i++) {
-            clearInterval(i);
+        for (let i = 0; i < intervalID.length; i++) {
+            clearInterval(intervalID[i]);
         }
         $('h1').text('You win!');
     } else {
-        setInterval(squareAnimation, intervalLength);
+        intervalID.push(setInterval(squareAnimation, intervalLength));
     }
 };
 
-$('h1').click(speedUpIntervalFunc);
+$('img').click(speedUpIntervalFunc);
